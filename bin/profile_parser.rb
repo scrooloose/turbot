5319e6fa844c001803e5fe2b2c95b77df5981238
Martin Grenfell <martin.grenfell@gmail.com>
2015-07-13 23:36:54 +0100

    spit out the interests that we parsed from the bio on the cmd line

--- bin/profile_parser.rb
+++ bin/profile_parser.rb
@@ -4,11 +4,15 @@ require File.dirname(__FILE__) + "/../lib/autopof"
 require 'pp'
 
 profile_page = open(ARGV.first).read
-
 profile = ProfileParser.new(page_content: profile_page).profile
 
 pp "Name: #{profile.name}"
+
 puts "\nInterests\n-------------"
 pp profile.interests
+
+puts "\nInterests parsed from bio\n-------------"
+pp BioParser.new(bio: profile.bio, interest_matchers: Topics::Base.all_interest_matchers).interests
+
 puts "\nBio\n-------------"
 pp profile.bio
