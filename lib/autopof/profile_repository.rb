616698581f1b3b12cb2dd1264224f073fd145323
Martin Grenfell <martin.grenfell@gmail.com>
2015-08-23 23:41:18 +0100

    stupid bugfix

--- lib/autopof/profile_repository.rb
+++ lib/autopof/profile_repository.rb
@@ -21,7 +21,7 @@ class ProfileRepository
   def messagable_profiles
     records = DB[:profiles].where("NOT EXISTS (SELECT * FROM messages WHERE messages.profile_id = profiles.id)")
     records.map do |record|
-      ProfileParser.new(page_content: record.page_content).profile
+      ProfileParser.new(page_content: record[:page_content]).profile
     end
   end
 
