e3fb6c9f9b686d7fe7775e56063f845d2d5a42eb
Martin Grenfell <martin.grenfell@gmail.com>
2015-08-24 00:28:42 +0100

    retarded bugfix

--- lib/autopof/message_repository.rb
+++ lib/autopof/message_repository.rb
@@ -5,7 +5,7 @@ class MessageRepository
 
   def save(profile: nil, message: nil)
     #TODO: make this not suck
-    profile_id = ProfileRepository.instance.find(pof_key: profile.pof_key).id
+    profile_id = ProfileRepository.instance.find(pof_key: profile.pof_key)[:id]
 
     DB[:messages].insert(content: message, profile_id: profile_id)
   end
