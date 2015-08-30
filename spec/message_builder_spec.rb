4f346254611b166354379806c8a15a2fc4d8f4ad
Martin Grenfell <martin.grenfell@gmail.com>
2015-08-30 15:23:25 +0100

    update a couple of outdated specs

--- spec/message_builder_spec.rb
+++ spec/message_builder_spec.rb
@@ -2,14 +2,14 @@ require File.dirname(__FILE__) + "/spec_helper"
 
 RSpec.describe MessageBuilder do
   describe "#message" do
-    it "opens with 'Hi [name]' if the profile has a name" do
+    it "opens with 'How's it going [name]?' if the profile has a name" do
       p = ProfileFactory.build(name: "Jane", interests: ["biking"])
-      expect(MessageBuilder.new(p).message).to start_with("Hi Jane")
+      expect(MessageBuilder.new(p).message).to start_with("How's it going Jane?")
     end
 
-    it "opens with 'Hi there!' if the profile has no name" do
+    it "opens with 'How's it going?' if the profile has no name" do
       p = ProfileFactory.build(name: nil, interests: ["biking"])
-      expect(MessageBuilder.new(p).message).to start_with("Hi there!")
+      expect(MessageBuilder.new(p).message).to start_with("How's it going?")
     end
 
     it "has a body from a topic" do
