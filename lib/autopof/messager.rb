bb98dbd067378cfd7dd0270eccb5b685921320e3
Martin Grenfell <martin.grenfell@gmail.com>
2015-09-26 17:08:17 +0100

    handle message sending errors
    Been getting a lot of these lately - and they halt the process
    altogether. Handle the so the program can continue

--- lib/autopof/messager.rb
+++ lib/autopof/messager.rb
@@ -12,9 +12,20 @@ class Messager
     Profile.messagable(message_limit).each do |profile|
       msg_text = MessageBuilder.new(profile).message
       Log.info("Messenger#go - Sending message to #{profile.username}. Message: #{msg_text}")
-      webdriver.send_message(message: msg_text, profile: profile) unless dry_run
+      attempt_to_send(msg_text, profile) unless dry_run
       sleep(rand(60) + 60) if sleep_between_msgs
     end
+  end
+
+private
+  def attempt_to_send(msg, profile)
+    webdriver.send_message(message: msg, profile: profile)
+  rescue StandardError => e
+    #vomit out stuff to stdout since cron will email that to us - i.e.
+    #effectively emailing us the error report
+    puts "Failed to send message to #{profile.username}"
+    puts e.message
+    puts e.backtrace
   end
 end
 
