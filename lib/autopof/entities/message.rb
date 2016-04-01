fbca765c937cceeb0de24c617d267f20fd1fb469
Martin Grenfell <martin.grenfell@gmail.com>
2016-04-01 16:34:20 +0100

    remove some old code from Message and add some pending specs to it

--- lib/autopof/entities/message.rb
+++ lib/autopof/entities/message.rb
@@ -1,10 +1,4 @@
 class Message < Sequel::Model(:messages)
-  subset(:with_response, 'response IS NOT NULL')
-
-  def self.messages_awaiting_response_for(username)
-    join(:profiles, id: :profile_id).where(profiles__username: username).where(response: nil).select(Sequel.lit('messages.*'))
-  end
-
   def self.create_sent_message(recipient: nil, content: nil)
     create(recipient_profile_id: recipient.id, content: content, sender_profile_id: Profile.me.id)
   end
--- spec/message_spec.rb
+++ spec/message_spec.rb
@@ -1,14 +1,8 @@
 require File.dirname(__FILE__) + "/spec_helper"
 
 RSpec.describe Message do
-  describe ".messages_awaiting_response_for" do
-    it "returns messages with no response" do
-      p = ProfileFactory.create
-      MessageFactory.create(response: "the response", responded_at: DateTime.now, profile: p)
-      m = MessageFactory.create(profile: p)
-
-      expect(Message.messages_awaiting_response_for(p.username).to_a).to eq([m])
-    end
-  end
+  pending ".create_sent_message does stuff"
+  pending ".create_received_message does stuff"
+  pending ".exists_for? does stuff"
 end
 
