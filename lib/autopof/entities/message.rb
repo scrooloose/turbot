0acfc5f33c579f731255902878b4a63c12a27cd0
Martin Grenfell <martin.grenfell@gmail.com>
2016-04-01 19:35:56 +0100

    set sent_at when sending messages

--- lib/autopof/entities/message.rb
+++ lib/autopof/entities/message.rb
@@ -1,6 +1,6 @@
 class Message < Sequel::Model(:messages)
   def self.create_sent_message(recipient: nil, content: nil)
-    create(recipient_profile_id: recipient.id, content: content, sender_profile_id: Profile.me.id)
+    create(recipient_profile_id: recipient.id, content: content, sender_profile_id: Profile.me.id, sent_at: Time.now)
   end
 
   def self.create_received_message(sender: nil, content: nil, sent_at: nil)
