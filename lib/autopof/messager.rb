fc36caac92f85cfd72e0261cf469c6284abf52c0
Martin Grenfell <martin.grenfell@gmail.com>
2016-04-02 17:24:09 +0100

    fix another circular ref

--- lib/autopof/messager.rb
+++ lib/autopof/messager.rb
@@ -1,7 +1,7 @@
 class Messager
   attr_reader :dry_run, :profiles, :webdriver, :sleep_between_msgs, :retries
 
-  def initialize(dry_run: true, profiles: profiles, webdriver: PofWebdriver::Base.new, sleep_between_msgs: AUTOPOF_ENV != "test", retries: 3)
+  def initialize(dry_run: true, profiles: nil, webdriver: PofWebdriver::Base.new, sleep_between_msgs: AUTOPOF_ENV != "test", retries: 3)
     @dry_run = dry_run
     @profiles = profiles
     @webdriver = webdriver
